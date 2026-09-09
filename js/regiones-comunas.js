3ecbb5f204490028eab01270ecbdb640ca6e0915
lobosz <di.loboso@duocuc.cl>
2026-09-09 20:52:51 -0300

    regiones

---
+++ js/regiones-comunas.js
@@ -0,0 +1,26 @@
+const regiones = [
+  {
+    nombre: "Región Metropolitana de Santiago",
+    comunas: ["Santiago", "Providencia", "Las Condes", "Maipú", "Puente Alto", "La Florida", "Ñuñoa"],
+  },
+  {
+    nombre: "Región de Valparaíso",
+    comunas: ["Valparaíso", "Viña del Mar", "Quilpué", "Concón", "San Antonio"],
+  },
+  {
+    nombre: "Región del Biobío",
+    comunas: ["Concepción", "Talcahuano", "Chillán", "Los Ángeles", "Coronel"],
+  },
+  {
+    nombre: "Región de la Araucanía",
+    comunas: ["Temuco", "Villarrica", "Angol", "Pucón"],
+  },
+  {
+    nombre: "Región de Ñuble",
+    comunas: ["Chillán", "San Carlos", "Bulnes"],
+  },
+  {
+    nombre: "Región de Los Lagos",
+    comunas: ["Puerto Montt", "Osorno", "Castro", "Puerto Varas"],
+  },
+];
